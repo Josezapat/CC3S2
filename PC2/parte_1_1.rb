12b7ec5ebc6bd0d44b7cf3fab2219196717de40e
Jose Zapata <90808325+Josezapat@users.noreply.github.com>
2023-10-11 08:31:04 -0500

    Update parte_1_1.rb

--- PC2/parte_1_1.rb
+++ PC2/parte_1_1.rb
@@ -1,1 +1,10 @@
+def encontrar_letra_faltante(cadena)
+  alfabeto = "abcdefghijklmnopqrstuvwxyz"
+  letras = cadena.downcase.scan(/[a-z]/)
+  letras_faltantes = alfabeto.chars - letras
+  letras_faltantes.first
+end
 
+cadena = "the quick brown box jumps over a lazy dog"
+letra_faltante = encontrar_letra_faltante(cadena)
+puts "La letra que falta es: #{letra_faltante}"
